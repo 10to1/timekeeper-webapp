25d07569bb794678b52e82a2e9a70754afae9a33
tomklaasen <tom@10to1.be>
2009-12-02 14:04:55 +0100

    untrack

--- app/controllers/keeps_controller.rb
+++ app/controllers/keeps_controller.rb
@@ -1,14 +1,22 @@
 class KeepsController < ApplicationController
   CONFIG_FILE = '/Users/tomklaasen/timekeeper.yml'
+  
   def index
     view do |tv|
       @keeps = tv.all
+      if params[:filter] == 'untracked'
+        @keeps.reject!{|k|k.tracked}
+      end
     end  
   end
   
   def update_tracked
     keeper do |tk|
-      tk.track(params[:pk], params[:name])
+      if params[:value] == 'true'
+        tk.track(params[:pk], params[:name])
+      else
+        untrack(params[:pk], params[:name])
+      end
     end
     
     render :nothing => true
@@ -25,5 +33,16 @@ class KeepsController < ApplicationController
     tk = Timekeeper::Keeper.new
     tk.config(CONFIG_FILE)
     yield(tk)
+  end
+  
+  def untrack(pk, name)
+    config = YAML.load_file(CONFIG_FILE)
+    tracking_table = Rufus::Tokyo::Table.new(File.join(config["db_path"],"tracking-time.tct"))
+    key = tracking_table.query{|q|
+      q.add 'name', :eq, name
+      q.add 'keep_id', :eq, pk 
+    }.first[:pk]
+    tracking_table.delete(key)
+    tracking_table.close
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def navigation
+    if params[:filter] == 'untracked'
+      link_to 'All', :params => {:filter => 'none'}
+    else
+      link_to 'Only untracked', :params => {:filter => 'untracked'}
+    end
+  end
 end
